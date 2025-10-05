de780d9760a39fbba633584df1bcfbc6463823e0
brettsky911 <brettsky911@gmail.com>
2025-10-05 00:18:23 -0400

    intial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import flask
+from flask import render_template
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
